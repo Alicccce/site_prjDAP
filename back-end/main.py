3b4ad043bf9e09a755038fc472fc72900713e190
ALICE <aliissa.mrak@mail.ru>
2026-04-04 13:59:08 +0300

    сервич автоиизации

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from services.auth_service import AuthService
+from repositories.user_repository import UserRepository
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello World"}
+user_repo = UserRepository()
+auth_service = AuthService(user_repo)
+
+@app.post("/register")
+def register(email: str, password: str, name: str):
+    return auth_service.register(email, password, name)
+
+@app.post("/login")
+def login(email: str, password: str):
+    return auth_service.login(email, password)
