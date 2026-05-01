f5f0041ca1805a81a5d080f33f03e0a9eebfc5c1
ALICE <aliissa.mrak@mail.ru>
2026-05-01 23:07:12 +0300

    правки для хостинга

--- front-end/src/api/axios.js
+++ front-end/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://site-prjdapdep.onrender.com', 
+  baseURL: 'https://site-prjdapdep.onrender.com/api', 
   })
 
 // Перехватчик для добавления токена
